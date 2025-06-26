4d08b5c7cd27ee6e2f117954228b4e895d562138
Tymek <vonautymek@gmail.com>
2025-06-26 23:59:53 +0200

    fix

--- apps/demo/babel.config.js
+++ apps/demo/babel.config.js
@@ -1,6 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { jsxImportSource: "@bacons/mdx/jsx" }]],
+    presets: [["babel-preset-expo", { jsxImportSource: "@tymekv/mdx/jsx" }]],
   };
 };
--- packages/mdx/default-metro-transformer.js
+++ packages/mdx/default-metro-transformer.js
@@ -1,5 +1,5 @@
 const upstreamTransformer = require("@expo/metro-config/babel-transformer");
-const MdxTransformer = require("@bacons/mdx/metro-transformer");
+const MdxTransformer = require("@tymekv/mdx/metro-transformer");
 
 module.exports.transform = async (props) => {
   // Then pass it to the upstream transformer.
